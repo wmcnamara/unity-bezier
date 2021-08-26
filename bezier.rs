42edc2e88b6ad43ff949270948a4cdce6d201921
Weston McNamara <westonfmcnamara@gmail.com>
2021-08-26 15:21:43 -0400

    Added cubic bezier and example

--- bezier.rs
+++ bezier.rs
@@ -30,12 +30,47 @@ fn quadratic_bezier(p1 : Point, p2 : Point, p3 : Point, t : f32) -> Point
     let a : Point = line_lerp(p1, p2, t);
     let b : Point = line_lerp(p2, p3, t);
 
-    let p : Point = line_lerp(a, b, t);
-
-    return p;
+    return line_lerp(a, b, t);
 }
 
+//Interpolates a cubic bezier curve with three control points, with the interpolant t.
+fn cubic_bezier(p1 : Point, p2 : Point, p3 : Point, p4 : Point, t : f32) -> Point 
+{
+    let a : Point = line_lerp(p1, p2, t);
+    let b : Point = line_lerp(p2, p3, t);
+    let c : Point = line_lerp(p3, p4, t);
+
+    let d : Point = line_lerp(a, b, t);
+    let e : Point = line_lerp(b, c, t);
+
+    return line_lerp(d, e, t);
+}
+
+/*
+    Testing
+*/
 fn main() 
 {
+    let p1 = Point { x : 10, y : 20};
+    let p2 = Point { x : 20, y : 5};
+    let p3 = Point { x : 30, y : 10};
+    let p3 = Point { x : 40, y : 50};
 
+    //Quadratic Bezier
+    for t in 0..100 
+    {
+        let t = t as f32 * 0.01;
+        Point bezierPoint = quadratic_bezier(p1, p2, p3, t);
+
+        //Drawing bezierPoint for each iteration will result in a quadratic bezier curve
+    }
+
+    //Quadratic Bezier
+    for t in 0..100 
+    {
+        let t = t as f32 * 0.01;
+        Point bezierPoint = cubic_bezier(p1, p2, p3, p4, t);
+
+        //Drawing bezierPoint for each iteration will result in a quadratic bezier curve
+    }
 }
